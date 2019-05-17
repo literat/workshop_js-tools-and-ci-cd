cbf25c41b372535182a40167b2599e0c80c57dd7
literat <tomas@litera.me>
2019-05-17 14:09:54 +0200

    webpack examples

---
+++ webpack/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: 'build/',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+      },
+      {
+        test: /\.svg$/,
+        loaders: ['file-loader', 'svgo-loader'],
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [autoprefixer],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
