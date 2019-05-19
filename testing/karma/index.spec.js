99b0f69aa65daead8236db9473eb8f7e22ef2eef
literat <tomas@litera.me>
2019-05-19 18:19:28 +0200

    karma testing example

---
+++ testing/karma/index.spec.js
@@ -0,0 +1,14 @@
+import { add, doComplicatedMath } from './calculator';
+
+describe('Calculator', function() {
+  it('should add two numbers', function() {
+    assert.equal(add(1, 2), 3);
+  });
+
+  it('should do some complicated math', function(done) {
+    doComplicatedMath(1, function(result) {
+      assert.equal(result, 1);
+      done();
+    });
+  });
+});
---
+++ testing/karma/karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'chai'],
+    files: ['./*.spec.js'],
+    preprocessors: {
+      './*.spec.js': ['webpack'],
+    },
+    webpack: {
+      mode: 'development',
+      devtool: 'source-map',
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /\.spec\.js$/,
+          },
+        ],
+      },
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['PhantomJS', 'ChromeHeadless'],
+    singleRun: true,
+  });
+};
